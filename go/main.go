96c9fb59048b8b5cdeda902efe888dae56da2757
Manuel de la Peña <mdelapenya@gmail.com>
2023-08-28 11:01:52 +0200

    chore: run application from console

--- go/main.go
+++ go/main.go
@@ -1,15 +1,36 @@
 package main
 
 import (
+	"flag"
 	"os"
-	"path/filepath"
 	"strings"
 )
 
 var tv Television = ConsoleTV{}
 
 func main() {
-	game := loadGame(filepath.Join("..", "game1.txt"))
+	//var defaultGameFile = filepath.Join("..", "game1.txt")
+
+	flag.Parse()
+	args := flag.Args()
+	if len(args) != 1 {
+		tv.Show("invalid number of arguments")
+		os.Exit(1)
+	}
+
+	gameFile := args[0]
+	if !strings.HasSuffix(gameFile, ".txt") {
+		tv.Show("invalid file name")
+		os.Exit(1)
+	}
+
+	// check if file exists
+	if _, err := os.Stat(gameFile); os.IsNotExist(err) {
+		tv.Show("file does not exist")
+		os.Exit(1)
+	}
+
+	game := loadGame(gameFile)
 
 	game.Play()
 }
